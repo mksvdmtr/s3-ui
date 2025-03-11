397bd229090c9641fc73dcf1143794e5b977f711
Dmitry Maksakov <dmitry.maksakov@lamoda.ru>
2025-03-11 13:38:58 +0300

    debug

--- app.py
+++ app.py
@@ -74,8 +74,8 @@ def list_objects():
             suffix = f'_ro'
             projects = [item.removeprefix(prefix).removesuffix(suffix) for item in groups]
             all_objects = []
-            print(groups)
-            print(projects)
+            logger.info(groups)
+            logger.info(projects)
             if projects:
                 for project in projects:
                     for key_prefix in KEY_PREFIXES.split(','):
